978fb32426f918cb861c91363e68d7d97b0cdf55
Guilherme Martin <99848607+guimartin7@users.noreply.github.com>
2026-09-24 21:45:52 -0300

    chore: export candle resampling helpers

--- src/mt5titan/marketdata/__init__.py
+++ src/mt5titan/marketdata/__init__.py
@@ -3,3 +3,5 @@
 from .providers import DemoMarketDataProvider, StoredMarketDataProvider, TwelveDataMarketDataProvider
 
 __all__ = ["DemoMarketDataProvider", "StoredMarketDataProvider", "TwelveDataMarketDataProvider"]
+
+from .resample import next_higher_timeframe, resample_candles
